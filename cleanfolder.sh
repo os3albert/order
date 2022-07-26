546de73bccec34904966b01cecc0aab3fe68d461
Albert <47365826+os3albert@users.noreply.github.com>
2022-07-26 18:30:57 +0200

    used to clean folder once you have used moveout

---
+++ cleanfolder.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mv ./auto_folder_java/* . 
+mv ./auto_folder_javascript/* . 
+mv ./auto_folder_python/* . 
+
+rmdir ./auto_folder_java
+rmdir ./auto_folder_javascript
+rmdir ./auto_folder_python
